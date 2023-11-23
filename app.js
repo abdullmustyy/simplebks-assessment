2a3d499123289b5de1b42d7a1f7ce38df6cad30a
abdullmustyy <abdulmusty03@outlook.com>
2023-11-23 18:25:08 +0100

    Feat: MongoDb connection setup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import "dotenv/config";
 import chalk from "chalk";
+// Database
+import { ObjectId } from "mongodb";
+import { connectMongoDb } from "./utils/mongodb.js";
 // File imports
 import { auth } from "./src/middlewares/auth.middleware.js";
 
@@ -14,7 +17,32 @@ app.use(auth);
 
 // Endpoints
 app.get("/", (req, res) => {
-  res.send("Protected route with Basic HTTP Authentication!");
+  res.send("Hello World!");
+});
+
+app.get("/seller", async (req, res) => {
+  try {
+    await connectMongoDb(async (database) => {
+      const olistSellersDataset = await database.collection(
+        "olist_sellers_dataset"
+      );
+      const seller = await olistSellersDataset.findOne({
+        _id: new ObjectId("655e701f74df0c1bc1447b9e"),
+      });
+
+      if (!seller) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Seller not found!" });
+      }
+
+      res
+        .status(200)
+        .json({ success: true, message: "Seller found!", data: seller });
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // Run the server
